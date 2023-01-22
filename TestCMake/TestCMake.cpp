303fe061d31c85b536e65185551ba9f75c6c50fa
Tiago2 <tiago8204@gmail.com>
2023-01-22 19:31:35 -0300

    Adicionar arquivos de projeto.

---
+++ TestCMake/TestCMake.cpp
@@ -0,0 +1,12 @@
+﻿// TestCMake.cpp: define o ponto de entrada para o aplicativo.
+//
+
+#include "TestCMake.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
