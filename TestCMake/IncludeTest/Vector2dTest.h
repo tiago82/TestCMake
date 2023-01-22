1e06b8b9cf21e6ffb7cc58b0a5e94de739233cd1
Tiago2 <tiago8204@gmail.com>
2023-01-22 20:19:40 -0300

    update

--- TestCMake/IncludeTest/Vector2dTest.h
+++ TestCMake/IncludeTest/Vector2dTest.h
@@ -1,1 +1,22 @@
 #pragma once
+
+class Vector2d
+{
+public:
+	void setX(float x_)
+	{
+		x = x_*2; // multiplicando por 2
+	}
+
+	float getX()
+	{
+		return x;
+	}
+
+private:
+	float x;
+	float y;
+};
+
+
+
--- TestCMake/TestCMake.cpp
+++ TestCMake/TestCMake.cpp
@@ -2,12 +2,19 @@
 //
 
 #include "TestCMake.h"
+#include "IncludeTest/Vector2dTest.h"
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+
 	cout << "Bem Vindo" << endl;
+
+	Vector2d vetor; // classe Vector2d do arquivo .h
+	vetor.setX(2);
+
+	cout << vetor.getX();
+
 	return 0;
 }
